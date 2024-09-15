7adfea897a45f377ac076ac08400c0d6b2736c73
Devin Lauderdale <devinlauderdale2@gmail.com>
2024-09-14 22:26:23 -0500

    test: update and move integration tests into src folder

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,8 +1,10 @@
 /**
  * Unit tests for the action's entrypoint, src/index.ts
+ *
+ * @jest-environment node
  */
 
-import * as main from '../src/main'
+import * as main from '../main'
 
 // Mock the action's entrypoint
 const runMock = jest.spyOn(main, 'run').mockImplementation()
@@ -10,7 +12,7 @@ const runMock = jest.spyOn(main, 'run').mockImplementation()
 describe('index', () => {
   it('calls run when imported', async () => {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('../src/index')
+    require('../../src/index')
 
     expect(runMock).toHaveBeenCalled()
   })
